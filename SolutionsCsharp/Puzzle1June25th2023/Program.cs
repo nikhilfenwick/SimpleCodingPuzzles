b1775e9179b51d895ad06f2a845dca58fd82e33d
Nikhil Fenwick <nikhilfenwick@hotmail.co.uk>
2023-07-26 11:21:39 +0100

    Puzzle 1 C SHARP Step 2.
    Just completed Step 2.

--- SolutionsCsharp/Puzzle1June25th2023/Program.cs
+++ SolutionsCsharp/Puzzle1June25th2023/Program.cs
@@ -6,14 +6,24 @@
 // 2. Make sure the random number is between 0 and 10.
 // 3. Display the random number on the output
 
+// Step Two
 
-int randomNumber (){
+// 1. Update the code in Step One, and turn it into a function called 'ReturnRandomNumber'.
+
+// Step Three
+
+// 1. Use the ReturnRandomNumber function to generate 99 random numbers.
+// 2. Store these 99 numbers in an array.
+// 1. Display the array.
+
+int ReturnRandomNumber (){
     Console.WriteLine("This function creates a random number between 1 and 10");
     Random rnd = new Random();
     int resultOfRandomNumber = rnd.Next(0, 10);
     return resultOfRandomNumber;
 }
 
-int resultOfFunction = randomNumber();
+int resultOfFunction = ReturnRandomNumber();
 Console.WriteLine(resultOfFunction);
+
 
