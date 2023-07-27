7f95d6ea762349c1205549f620f08238433fd056
Nikhil Fenwick <nikhilfenwick@hotmail.co.uk>
2023-07-27 12:04:32 +0100

    Puzzle 2 C SHARP Step 1.
    Starting Puzzle 2.

---
+++ SolutionsCsharp/Puzzle2June27th2023/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Let's build a simple calculator -
+
+// # Things To Do - 
+
+// (note: output should always be stored in a double variable)
+
+// Step One - 
+
+// 1. Create two integer variables.
+// 2. Create a output variable.
+// 3. Add the two integers and store the output in the output variable.
+// 4. Repeat step 2 with subtraction.
+// 5. Repeat step 2 with multiplication.
+// 6. Repeat step 3 with division (no need to check for divide by zero).
+
+int firstInt = 5;
+int secondInt = 6;
+
+int additionOfInt = firstInt + secondInt;
+int subtratcionOfInt = firstInt - secondInt; 
+int multiplicationOfInt = firstInt * secondInt; 
+int divisionOfInt = firstInt / secondInt; 
+
+Console.WriteLine(additionOfInt);
+Console.WriteLine(subtratcionOfInt);
+Console.WriteLine(multiplicationOfInt);
+Console.WriteLine(divisionOfInt);
